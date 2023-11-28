5ff244e454cb3afe8d3b1ddb6db9c76828a3275a
lauryin boke <lauryinboke6@gmail.com>
2023-11-28 21:56:07 +0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+class Program
+{
+//lets see if this will work
+static void Main(string[] args)
+{
+    //set variables:
+
+    int num1;
+    int num2;
+    int result;
+    string operation;
+
+    Console.WriteLine("Calculator");
+    Console.ReadLine();
+
+    //get input from users:
+
+    Console.WriteLine("Enter Your First Number : ");
+    num1 = Convert.ToInt32(Console.ReadLine());
+
+     Console.WriteLine("Enter Your Second Number : ");
+    num2 = Convert.ToInt32(Console.ReadLine());
+
+    //get operation type
+
+    Console.WriteLine("Which kind of operation do you want ! (+ , - , / , * )");
+    operation = Console.ReadLine();
+
+    //conditions on operation:
+
+    if (operation == "+"){
+        result = num1 + num2;
+        Console.WriteLine(num1 + "+" + num2 + "=" + result);
+        Console.ReadLine();
+    }
+
+    else if (operation == "-"){
+        result = num1 - num2;
+        Console.WriteLine(num1 + "-" + num2 + "=" + result);
+        Console.ReadLine();
+    }
+
+    else if (operation == "/"){
+        result = num1 / num2;
+        Console.WriteLine(num1 + "/" + num2 + "=" + result);
+        Console.ReadLine();
+    }
+
+    else if (operation == "*"){
+        result = num1 * num2;
+        Console.WriteLine(num1 + "*" + num2 + "=" + result);
+        Console.ReadLine();
+    }
+
+    else {
+        Console.WriteLine("Select the correct operation");
+        Console.ReadLine();
+    }
+
+
+}
+}
+
